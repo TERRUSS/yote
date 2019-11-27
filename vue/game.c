6d35d898952adf74645000417f99e3a0834a9d67
TERRUSS <olivier.benaben@protonmail.com>
2019-11-27 09:11:41 +0100

    added template

--- vue/game.c
+++ vue/game.c
@@ -1,37 +1,42 @@
 #include "header.h"
 
 void print_pawn(SDL_Window* pWindow){
-  SDL_Renderer *pRenderer = SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED); // Création d'un SDL_Renderer utilisant l'accélération matérielle
-  SDL_SetRenderDrawColor(pRenderer, 255, 0, 0, 255);
+	SDL_Renderer *pRenderer = SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED); // Création d'un SDL_Renderer utilisant l'accélération matérielle
+	// SDL_SetRenderDrawColor(pRenderer, 255, 0, 0, 255);
 
-  if ( pRenderer ) {
-    SDL_Surface* pSprite = IMG_Load("./assets/block/block.png");
+	if ( pRenderer ) {
+		SDL_Surface* pSprite = IMG_Load("./assets/block/block.png");
+		printf("Sprite : w=%d, d=%d, dest = %dx%d\n", pSprite->w, pSprite->h, 800/2 - pSprite->w/2,600/2 - pSprite->h/2);
+		SDL_Delay(50);
 
-    if ( pSprite ) {
-      SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer,pSprite); // Préparation du sprite
+		if ( pSprite ) {
+			SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer, pSprite); // Préparation du sprite
 
-      if ( pTexture ) {
-        SDL_Rect dest = { 640/2 - pSprite->w/2,480/2 - pSprite->h/2, pSprite->w, pSprite->h};
-        SDL_RenderCopy(pRenderer, pTexture, NULL, &dest); // Copie du sprite grâce au SDL_Renderer
-        SDL_RenderPresent(pRenderer); // Affichage
-        printf("debut\n");
-        SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voit la fenêtre */
-        printf("fin\n");
-        SDL_DestroyTexture(pTexture); // Libération de la mémoire associée à la texture
+			if ( pTexture ) {
+				SDL_Rect dest = { 800/2 - pSprite->w/2, 600/2 - pSprite->h/2, pSprite->w, pSprite->h};
 
-      } else {
-        fprintf(stdout,"[SDL CreateTextureFromSurface] (%s)\n",SDL_GetError());
-      }
+				SDL_RenderCopy(pRenderer, pTexture, NULL, &dest); // Copie du sprite grâce au SDL_Renderer
+				SDL_RenderPresent(pRenderer); // Affichage
 
-      SDL_FreeSurface(pSprite); // Libération de la ressource occupée par le sprite
+				printf("debut\n");
+				SDL_Delay(1000); /* Attendre trois secondes, que l'utilisateur voit la fenêtre */
+				printf("fin\n");
 
-    } else {
-      fprintf(stdout,"[SDL LoadBMP] (%s)\n",SDL_GetError());
-    }
+				SDL_DestroyTexture(pTexture); // Libération de la mémoire associée à la texture
 
-    SDL_DestroyRenderer(pRenderer); // Libération de la mémoire du SDL_Renderer
+			} else {
+				fprintf(stdout,"[SDL CreateTextureFromSurface] (%s)\n",SDL_GetError());
+			}
 
-  } else {
-    fprintf(stdout,"Échec de création du renderer (%s)\n",SDL_GetError());
-  }
+			SDL_FreeSurface(pSprite); // Libération de la ressource occupée par le sprite
+
+		} else {
+			fprintf(stdout,"[SDL Load Sprite] (%s)\n",SDL_GetError());
+		}
+
+		SDL_DestroyRenderer(pRenderer); // Libération de la mémoire du SDL_Renderer
+
+	} else {
+		fprintf(stdout,"[SDL Renderer] (%s)\n",SDL_GetError());
+	}
 }
